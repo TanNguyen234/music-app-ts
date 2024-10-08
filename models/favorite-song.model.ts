f498a625b41312125df3da57dcb6bed1692bd49f
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-08 16:41:10 +0700

    Tạo collection favorite-songs

---
+++ models/favorite-song.model.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const favoriteSongSchema = new mongoose.Schema({//Thiết lập schema
+  userId: String,
+  songId: String,
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deleteAt: Date
+}, {timestamps: true});//Hàm mongoose nếu giá trị là true thì nó sẽ tự động lưu lại [ngày tạo] và khi uppdate nó cg tự động lưu [ngày updata]
+
+const FavoriteSong = mongoose.model("FavoriteSong", favoriteSongSchema, "favorite-songs"); //Kết nối tới collection có tên products
+
+export default FavoriteSong;
