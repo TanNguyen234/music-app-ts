80ef6057aecd296265af7b080e65069bc4130f11
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-12 20:01:52 +0700

    Tinh nang nghe thu audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -4,7 +4,7 @@ const uploadImage = document.querySelector('[upload-image]');
 if(uploadImage) {
     const uploadImageInput = uploadImage.querySelector('[upload-image-input]');
     const uploadImagePreview = uploadImage.querySelector('[upload-image-preview]');
-    const update = document.querySelector('[update]');
+    const update = uploadImage.querySelector('[update]');
 
     if(!uploadImageInput.value) {
         update.style.display = 'none';
@@ -21,7 +21,6 @@ if(uploadImage) {
         }
 
         x.addEventListener('click', (e) => {
-            console.log("x");
             uploadImagePreview.src = ""
             uploadImage.children[2].style.display = 'none';
             uploadImageInput.value = "";
@@ -29,3 +28,38 @@ if(uploadImage) {
     })
 }
 //End Upload Image
+//Upload Audio
+const uploadAudio = document.querySelector('[upload-audio]');
+
+if(uploadAudio) {
+    const uploadAudioInput = uploadAudio.querySelector('[upload-audio-input]');
+    const uploadAudioPlay = uploadAudio.querySelector('audio[upload-audio-play]');
+    const source = uploadAudioPlay.querySelector('source');
+    const update = uploadAudio.querySelector('[update]');
+
+    if(uploadAudioInput.value === '' | undefined | null) {
+        update.style.display = 'none';
+        source.src = ""
+        uploadAudioInput.value = "";
+    }
+
+    uploadAudioInput.addEventListener('change', (e) => {
+        uploadAudio.children[2].style.display = 'flex';
+        let file = e.target.files[0];
+        const x = uploadAudio.children[2].children[1];
+        
+        if(file) {
+            source.src = URL.createObjectURL(file)//Hàm tạo đường dẫn
+            uploadAudioPlay.load()
+        }
+
+        console.log(x)
+
+        x.addEventListener('click', (e) => {
+            update.style.display = 'none';
+            source.src = ""
+            uploadAudioInput.value = "";
+        })
+    })
+}
+//End Upload Audio
